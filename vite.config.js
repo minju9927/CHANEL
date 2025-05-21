b86ba01003653518e3288dc5c8028e3fe01f90a5
minju9927 <pink5214@naver.com>
2025-05-21 12:32:50 +0900

    1

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(
-    
-  )],
+  base: '/CHANEL/', // 필수: GitHub repo 이름
+  build: {
+    outDir: 'docs', 
+  },
+  plugins: [react()],
   css: {
     devSourcemap: true  // ✅ 요게 핵심!
   }
